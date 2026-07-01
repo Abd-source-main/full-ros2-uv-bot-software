0a144901177cc261799745b50ab29236e178e1f6
Abd-source-main <abdllaabody1234@gmail.com>
2026-07-01 21:42:37 +0300

    added setup.py

---
+++ setup.py
@@ -0,0 +1,36 @@
+from setuptools import find_packages, setup
+from glob import glob
+import os
+
+package_name = 'the_robot'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name, 'launch'), glob('launch/*.launch.py')),
+        (os.path.join('share', package_name, 'config'), glob('config/*')),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='eng-abd',
+    maintainer_email='eng-abd@todo.todo',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'nav2_goal = the_robot.nav2_goal:main',
+            'run_gazebo = the_robot.run_gazebo:main',
+            'saif = the_robot.saif:main',
+            'slam = the_robot.slam:main',
+            'spawn_robot = the_robot.spawn_robot:main',
+            'teleop_wasd = the_robot.teleop_wasd:main',
+            'web_goal = the_robot.web_goal:main',
+        ],
+    },
+)
